d8fbebf47a29e2cdcb70a390b8f646ee9d11cdd9
samy884 <ssgill884@gmail.com>
2022-05-30 10:38:38 +0530

    using useeffect for fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,8 +8,9 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMovieHandler() {
+  const fetchMovieHandler = useCallback(async () => {
     setIsLoading(true);
+    setError(null);
     try {
       const response = await fetch("https://swapi.dev/api/films/");
       if (!response.ok) {
@@ -30,7 +31,11 @@ function App() {
       setError(error.message);
       setIsLoading(false);
     }
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchMovieHandler();
+  }, [fetchMovieHandler]);
 
   let content = <p>No movie found.</p>;
 
